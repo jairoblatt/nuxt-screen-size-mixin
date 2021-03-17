835ae454b3db087f04d5fd5341867411dd91a6b9
Jairo Blatt <jairoblattc@gmail.com>
2021-03-17 07:30:12 -0300

    Create nuxt-screen-size-mixin.js

---
+++ nuxt-screen-size-mixin.js
@@ -0,0 +1,48 @@
+import Vue from "vue";
+
+var nuxtScreenSizeMixin = {
+  data: () => ({
+    nuxtWidth: 0,
+    nuxtHeight: 0
+  }),
+
+  mounted() {
+    this.resize();
+
+    window.addEventListener("resize", this.resize);
+
+    this.$once("hook:beforeDestroy", function() {
+      window.removeEventListener("resize", this.resize);
+    });
+  },
+
+  methods: {
+    getWidth() {
+      if (process.client) {
+        return (
+          window.innerWidth ||
+          document.documentElement.clientWidth ||
+          document.body.clientWidth
+        );
+      }
+    },
+
+    getHeight() {
+      if (process.client) {
+        return (
+          window.innerHeight ||
+          document.documentElement.clientHeight ||
+          document.body.clientHeight
+        );
+      }
+    },
+
+    resize() {
+      this.nuxtWidth = this.getWidth();
+      this.nuxtHeight = this.getHeight();
+    }
+  }
+};
+
+export default nuxtScreenSizeMixin;
+Vue.mixin(nuxtScreenSizeMixin);
